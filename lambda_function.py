8ea049325164c7fd944c4501f4f485d3df0a9dd7
Thomas Stansel <thomas.s.stansel@gmail.com>
2021-03-07 08:09:34 -0700

    Working with the alexa skill -> hung up on getting the chrome driver to work in lambda env

--- lambda_function.py
+++ lambda_function.py
@@ -1,80 +1,236 @@
-from selenium import webdriver 
-import time
-import re
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import os
+
+import logging
+import ask_sdk_core.utils as ask_utils
+
 from ask_sdk_core.skill_builder import SkillBuilder
 from ask_sdk_core.dispatch_components import AbstractRequestHandler
 from ask_sdk_core.dispatch_components import AbstractExceptionHandler
-from ask_sdk_core.utils import is_request_type, is_intent_name
+from ask_sdk_core.handler_input import HandlerInput
 
-class LaunchReqeustHandler(AbstractRequestHandler):
+from ask_sdk_model import Response
+from utils import create_presigned_url
+
+import time
+import re
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+class LaunchRequestHandler(AbstractRequestHandler):
+    """Handler for Skill Launch."""
     def can_handle(self, handler_input):
-        return is_request_type("LaunchRequest")(handler_input)
+        # type: (HandlerInput) -> bool
+
+        return ask_utils.is_request_type("LaunchRequest")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speak_output = "Welcome, please ask Alexa What is my universities caps number to get help"
+
+        return (
+            handler_input.response_builder
+                .speak(speak_output)
+                .ask(speak_output)
+                .response
+        )
+
+
+class HelloWorldIntentHandler(AbstractRequestHandler):
+    """Handler for Hello World Intent."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return ask_utils.is_intent_name("HelloWorldIntent")(handler_input)
+
+    def handle(self, handler_input):
+        speak_output = "You can say hello to me! How can I help?"
+
+        return (
+            handler_input.response_builder
+                .speak(speak_output)
+                # .ask("add a reprompt if you want to keep the session open for the user to respond")
+                .response
+        )
     
-    def handle(self,handler_input):
-        handler_input.response_builder.speak("Searching now.".set_should_end_session(False))
+
+
+class HelpIntentHandler(AbstractRequestHandler):
+    """Handler for Help Intent."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return ask_utils.is_intent_name("AMAZON.HelpIntent")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speak_output = "You can say hello to me! How can I help?"
+
+        return (
+            handler_input.response_builder
+                .speak(speak_output)
+                .ask(speak_output)
+                .response
+        )
+
+
+class CancelOrStopIntentHandler(AbstractRequestHandler):
+    """Single handler for Cancel and Stop Intent."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return (ask_utils.is_intent_name("AMAZON.CancelIntent")(handler_input) or
+                ask_utils.is_intent_name("AMAZON.StopIntent")(handler_input))
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speak_output = "Goodbye!"
+
+        return (
+            handler_input.response_builder
+                .speak(speak_output)
+                .response
+        )
+
+
+class SessionEndedRequestHandler(AbstractRequestHandler):
+    """Handler for Session End."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return ask_utils.is_request_type("SessionEndedRequest")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+
+        # Any cleanup logic goes here.
+
         return handler_input.response_builder.response
+
+
+class IntentReflectorHandler(AbstractRequestHandler):
+    """The intent reflector is used for interaction model testing and debugging.
+    It will simply repeat the intent the user said. You can create custom handlers
+    for your intents by defining them above, then also adding them to the request
+    handler chain below.
+    """
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return ask_utils.is_request_type("IntentRequest")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        intent_name = ask_utils.get_intent_name(handler_input)
+        speak_output = "You just triggered " + intent_name + "."
+
+        return (
+            handler_input.response_builder
+                .speak(speak_output)
+                # .ask("add a reprompt if you want to keep the session open for the user to respond")
+                .response
+        )
+
 
 class CatchAllExceptionHandler(AbstractExceptionHandler):
-    def can_handle(self,handler_input,exception):
+    """Generic error handling to capture any syntax or routing errors. If you receive an error
+    stating the request handler chain is not found, you have not implemented a handler for
+    the intent being invoked or included it in the skill builder below.
+    """
+    def can_handle(self, handler_input, exception):
+        # type: (HandlerInput, Exception) -> bool
         return True
-    def handle(self,handler_input,exception):
-        print(exception)
-        handler_input.response_builder.speak("Something went wrong. Please try again.")
-        return handler_input.response_builder.response
+
+    def handle(self, handler_input, exception):
+        # type: (HandlerInput, Exception) -> Response
+        logger.error(exception, exc_info=True)
+
+        speak_output = "Sorry, I had trouble doing what you asked. Please try again."
+
+        return (
+            handler_input.response_builder
+                .speak(speak_output)
+                .ask(speak_output)
+                .response
+        )
+
+# The SkillBuilder object acts as the entry point for your skill, routing all request and response
+# payloads to the handlers above. Make sure any new handlers or interceptors you've
+# defined are included below. The order matters - they're processed top to bottom.
 
 class UniversityMHNumberIntentHandler(AbstractRequestHandler):
     def can_handle(self,handler_input):
-        return is_intent_name("UniversityMHNumberIntent")(handler_input)
+        return ask_utils.is_intent_name("UniversityMHNumberIntent")(handler_input)
     
     def handle(self,handler_input):
-        uni = handler_input.request_envelope.request.intent.slots['university'].value
-        college = handler_input.request_envelope.request.intent.slots['college'].value
-        if uni == None:
-            uni = college
-        phones = findNumber(uni)
+        uni = handler_input.request_envelope.request.intent.slots['universities'].value
+        chromeDriver = create_presigned_url("Media/chromedriver")
+        phones = self.findNumber(uni,chromeDriver)
         speech_text = phones[0]
         handler_input.response_builder.speak(speech_text).set_should_end_session(False)
         return handler_input.response_builder.response
+        
+    def findNumber(self,university,cd):
+        
+        # Taking input from user through alexa and add it to this
+        userInput = university
+        searchString = userInput+"+Counseling+and+Psychological+Services"
+        
+        #Format the String if needed
+        searchString = searchString.replace(' ', '+')
+        
+        #Open Chrome
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--window-size=1280x1696')
+        chrome_options.add_argument('--user-data-dir=/tmp/user-data')
+        chrome_options.add_argument('--hide-scrollbars')
+        chrome_options.add_argument('--enable-logging')
+        chrome_options.add_argument('--log-level=0')
+        chrome_options.add_argument('--v=99')
+        chrome_options.add_argument('--single-process')
+        chrome_options.add_argument('--data-path=/tmp/data-path')
+        chrome_options.add_argument('--ignore-certificate-errors')
+        chrome_options.add_argument('--homedir=/tmp')
+        chrome_options.add_argument('--disk-cache-dir=/tmp/cache-dir')
+        chrome_options.add_argument('user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36')
+        chrome_options.binary_location = os.getcwd() + "/bin/"+cd
+        
+        browser = webdriver.Chrome(chrome_options=chrome_options)
+            
+        #Go to the google search results
+        matched_elements = browser.get("https://www.google.com/search?q=" +searchString)
+        time.sleep(3)
+        
+        #Get the first result and click it
+        results = browser.find_element_by_tag_name('h3')
+        results.click()
+        
+        #Get the pages HTMl and using regex get all possible phone numbers
+        doc = browser.page_source
+        phones = re.findall(r'(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4})',doc)
+        
+        #Do some more proofing to cut the numbers down
+        proofedPhones = []
+        for phone in phones:
+            if phone.rfind("(") != -1:
+                proofedPhones.append(phone)
+            elif phone.rfind(".") != -1:
+                proofedPhones.append(phone)
+        
+        return proofedPhones
+    
 
 sb = SkillBuilder()
-sb.add_request_handler(LaunchReqeustHandler())
-sb.add_exception_handler(CatchAllExceptionHandler())
+
+sb.add_request_handler(LaunchRequestHandler())
+sb.add_request_handler(HelloWorldIntentHandler())
+sb.add_request_handler(HelpIntentHandler())
 sb.add_request_handler(UniversityMHNumberIntentHandler())
+sb.add_request_handler(CancelOrStopIntentHandler())
+sb.add_request_handler(SessionEndedRequestHandler())
+sb.add_request_handler(IntentReflectorHandler()) # make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
 
+sb.add_exception_handler(CatchAllExceptionHandler())
 
-def lambda_handler(event,context):
-    return sb.lambda_handler()(event,context)
-
-def findNumber(university):
-    # Taking input from user through alexa and add it to this
-    userInput = ""
-    searchString = userInput+"+Counseling+and+Psychological+Services"
-
-    #Format the String if needed
-    searchString = searchString.replace(' ', '+') 
-
-    #Open Chrome
-    browser = webdriver.Chrome() 
-
-    #Go to the google search results
-    matched_elements = browser.get("https://www.google.com/search?q=" +searchString) 
-    time.sleep(3)
-
-    #Get the first result and click it
-    results = browser.find_element_by_tag_name('h3')
-    results.click()
-
-    #Get the pages HTMl and using regex get all possible phone numbers
-    doc = browser.page_source
-    phones = re.findall(r'(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4})',doc)
-
-    #Do some more proofing to cut the numbers down
-    proofedPhones = []
-    for phone in phones:
-        if phone.rfind("(") != -1:
-            proofedPhones.append(phone)
-        elif phone.rfind(".") != -1:
-            proofedPhones.append(phone)
-    
-    return proofedPhones
-
+lambda_handler = sb.lambda_handler()
