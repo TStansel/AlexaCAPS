88b59eadc6934bb718e6034417dd34efaecda1ea
Thomas Stansel <thomas.s.stansel@gmail.com>
2021-03-06 17:32:11 -0700

    Undid an accidental delete

--- alexaCaps.py
+++ alexaCaps.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 import time
 
 # Taking input from user 
-search_string = "Harvard+Counseling+and+Psychological+Services"
+search_string = "BYU+Counseling+and+Psychological+Services"
 
 # This is done to structure the string 
 # into search url.(This can be ignored) 
@@ -15,5 +15,5 @@ browser = webdriver.Chrome()
 matched_elements = browser.get("https://www.google.com/search?q=" +search_string) 
 time.sleep(3)
 results = browser.find_element_by_tag_name('h3')
-
+results.click()
 
